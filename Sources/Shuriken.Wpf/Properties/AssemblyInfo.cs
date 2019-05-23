3e98b182a88371b5ba2ab4c4b210640c229f89a2
Michael Damatov <md@netfile.info>
2019-05-23 22:51:42 +0200

    make assembly CLS-Compliant

--- Sources/Shuriken.Wpf/Properties/AssemblyInfo.cs
+++ Sources/Shuriken.Wpf/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Windows.Markup;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("50F3B06C-90C5-4BFA-8652-F635959B7CFE")]
